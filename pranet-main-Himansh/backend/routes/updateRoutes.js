2155a7cb7a3eabe00c01e8ebfc02028a8f3a871a
Himanshu Kushwaha <himanshuji0563@gmail.com>
2026-03-29 14:41:20 +0530

    Delete pranet-main-Himansh directory

--- pranet-main-Himansh/backend/routes/updateRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { addUpdate, getUpdates } = require("../controllers/updateController");
-
-router.post("/", addUpdate);
-router.get("/:issueId", getUpdates);
-
-module.exports = router;
--- pranet-main-Himansh/src/components/ai-support/index.ts
+++
@@ -1,9 +0,0 @@
-export { AICopilotPanel } from "./AICopilotPanel";
-export { ProblemCard, CIVIC_PROBLEMS } from "./ProblemCard";
-export type { CivicProblem } from "./ProblemCard";
-export { KnowledgeBasePDF } from "./KnowledgeBasePDF";
-export type { PDFContext } from "./KnowledgeBasePDF";
-export { ResourcePanel } from "./ResourcePanel";
-export { AIChatMessages } from "./AIChatMessages";
-export type { ChatMessage } from "./AIChatMessages";
-export { AIChatInput } from "./AIChatInput";
